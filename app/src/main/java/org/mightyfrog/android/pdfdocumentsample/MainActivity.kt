788cc55bafdff912e12add31302988a05f921a37
mightyfrog <mightyfrog.gc@gmail.com>
2018-09-11 02:08:33 +0900

    Initial commit

---
+++ app/src/main/java/org/mightyfrog/android/pdfdocumentsample/MainActivity.kt
@@ -0,0 +1,45 @@
+package org.mightyfrog.android.pdfdocumentsample
+
+import android.graphics.pdf.PdfDocument
+import android.os.Bundle
+import android.os.Environment
+import android.util.DisplayMetrics
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+
+
+/**
+ * @author Shigehiro Soejima
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            generatePdf()
+        }
+    }
+
+    private fun generatePdf() {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val w = displayMetrics.widthPixels
+        val h = displayMetrics.heightPixels
+        PdfDocument().apply {
+            val page = startPage(PdfDocument.PageInfo.Builder(w, h, 1).create())
+            val view = findViewById<View>(android.R.id.content)
+            view.draw(page.canvas)
+            finishPage(page)
+            val f = File(Environment.getExternalStorageDirectory(), "test.pdf")
+            FileOutputStream(f).use {
+                writeTo(it)
+            }
+        }
+    }
+}
